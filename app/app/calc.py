2425c1607e42a9cd063344e02fb5be2a36222e57
dilippoudel <dilippoudel578@gmail.com>
2024-10-26 10:20:17 +0300

    Added another testcase demo

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,4 +1,11 @@
 """This is the calucator module."""
+
+
 def add(a, b):
     """Sum a and b and return the result."""
     return a + b
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
